d6a12d86bf53a41864ef272a6d8cd1154c611f65
Michael Nelson <thespacecamel@hotmail.com>
2020-08-11 21:55:18 -0700

    add post thumbnails to design

--- default_designs/classic/section.php
+++ default_designs/classic/section.php
@@ -20,6 +20,11 @@
     </header><!-- .entry-header -->
 
     <div class="post-inner">
+        <?php
+        if ( has_post_thumbnail() ) {
+            the_post_thumbnail('large');
+        }
+        ?>
         <div class="entry-content post-content">
             <?php the_content();?>
         </div><!-- .entry-content -->
