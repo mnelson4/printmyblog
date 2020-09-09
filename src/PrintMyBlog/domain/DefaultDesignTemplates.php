7f4095a2b255f0562321dd56fbc891baaca79d63
Michael Nelson <thespacecamel@hotmail.com>
2020-09-09 09:48:20 -0700

    add pritn pdf format and a default design for it

--- src/PrintMyBlog/domain/DefaultDesignTemplates.php
+++ src/PrintMyBlog/domain/DefaultDesignTemplates.php
@@ -10,16 +10,33 @@ use Twine\forms\inputs\YesNoInput;
 class DefaultDesignTemplates {
 	public function registerDesignTemplates()
 	{
-//		pmb_register_design_template(
-//			'classic_print',
-//			[
-//				'title' => __('Classic', 'print-my-blog'),
-//				'format' => 'print_pdf',
-//				'dir' => PMB_DEFAULT_DESIGNS_DIR . '/classic_print',
-//				'url' => PMB_DEFAULT_DESIGNS_URL . '/classic_print',
-//				'design_options' =>
-//			]
-//		);
+		pmb_register_design_template(
+			'classic_print',
+			function() {
+				return [
+					'title'                 => __( 'Classic Print PDf', 'print-my-blog' ),
+					'format'                => 'print_pdf',
+					'dir'                   => PMB_DEFAULT_DESIGNS_DIR . '/classic_digital',
+					'design_form_callback'  => function () {
+						return new FormSectionProper( [
+							'subsections' => [
+								'show_title' => new YesNoInput()
+							]
+						] );
+					},
+					'project_form_callback' => function ( Design $design ) {
+						$sections = [];
+						if ( $design->getPmbMeta( 'show_title' ) ) {
+							$sections['title'] = new TextInput();
+						}
+
+						return new FormSectionProper( [
+							'subsections' => $sections
+						] );
+					}
+				];
+			}
+		);
 		pmb_register_design_template(
 			'classic_digital',
 			function() {
--- src/PrintMyBlog/domain/DefaultDesigns.php
+++ src/PrintMyBlog/domain/DefaultDesigns.php
@@ -10,6 +10,23 @@ class DefaultDesigns {
 	public function registerDefaultDesigns()
 	{
 		pmb_register_design(
+			'classic_print',
+			'classic_print_pdf',
+			function(DesignTemplate $design_template){
+				return [
+					'title' => __('Classic Print PDF', 'print-my-blog'),
+					'description' => __('Look inspired by Print My Blogs original, containing a quick printout heading and compact design.', 'print-my-blog'),
+					'featured_image' => plugins_url($design_template->getDir() . '/preview.png'),
+					'design_defaults' => [
+						'use_title' => true,
+					],
+					'project_defaults' => [
+						'title' => get_bloginfo('name')
+					]
+				];
+			}
+		);
+		pmb_register_design(
 			'classic_digital',
 			'classic_digital_pdf',
 			function(DesignTemplate $design_template){
