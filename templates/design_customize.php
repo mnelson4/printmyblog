852bdf86270e54b49b13679810472e1453f06eab
Michael Nelson <thespacecamel@hotmail.com>
2020-11-09 11:46:01 -0800

    dont flex columns

--- templates/design_customize.php
+++ templates/design_customize.php
@@ -22,7 +22,6 @@ pmb_render_template(
 	]
 );
 ?>
-<div><span class="pmb-help"><?php printf(__('Design supports %d layers of nested divisions', 'print-my-blog'), $design->getDesignTemplate()->getLevels());?></span></div>
 <form method="POST" action="<?php echo esc_attr($form_url);?>">
     <?php echo $form->get_html_and_js();?>
     <button class="button button-primary pmb-save"><?php esc_html_e('Save', 'print-my-blog');?></button>
