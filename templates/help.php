46dd7c78d8f7701caf55ff4c7bd28ee393357726
Michael Nelson <thespacecamel@hotmail.com>
2021-01-25 12:23:44 -0800

    add link from help page too

--- templates/help.php
+++ templates/help.php
@@ -6,6 +6,11 @@
 ?>
 <div class="pmb-after-trouble">
     <h2><?php esc_html_e('We’re Here to Help!', 'print-my-blog');?></h2>
+    <p><?php printf(
+            esc_html__('Please %1$stake a look at our user guide%2$s for quick answers. If that doesn’t contain the info you need, please get in touch.', 'print-my-blog'),
+        '<a href="https://printmy.blog/user-guide/" target="_blank">',
+        '</a>'
+        );?></p>
     <form method="POST" action="<?php echo esc_attr($form_url);?>">
     <?php echo $form->getHtmlAndJs();?>
     <div>
