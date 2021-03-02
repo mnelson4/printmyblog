4107f161c1112179e52c10a92fea33a26b5c65d0
Michael Nelson <thespacecamel@hotmail.com>
2021-03-02 10:56:27 -0800

    tell users data is sent to external service for conversion

--- src/PrintMyBlog/domain/DefaultPersistentNotices.php
+++ src/PrintMyBlog/domain/DefaultPersistentNotices.php
@@ -120,11 +120,15 @@ class DefaultPersistentNotices
             new Notice(
                 'pmb_generate',
                 __('Preview and Generate Your File', 'print-my-blog'),
-                '<p>' . __('Your project is ready to be generated! Use one of the buttons below to generate it, but feel free to use the links above to tweak it.', 'print-my-blog') .
+                '<p>' . sprintf(
+                    __('Your project is ready to be generated! Clicking "Download Preview" will %1$ssend your project data to printmy.blog and docraptor.com%2$s for converting into a watermarked PDF. ', 'print-my-blog'),
+                    '<a href="https://printmy.blog/user-guide/getting-started/privacy-and-pmb-pro/" target="_blank">',
+                '</a>'
+                ).
                 '</p>'
                 . '<p>'
                 . sprintf(
-                    __('Read the User Guide on %1$spreviewing%2$s, %3$supdating%2$s, %4$sgenerating the pro file%2$s, and %5$sgetting help%2$s.', 'print-my-blog'),
+                    __('Read the User Guide on how your data is secured, %1$spreviewing%2$s, %3$supdating%2$s, %4$sgenerating the pro file%2$s, and %5$sgetting help%2$s.', 'print-my-blog'),
                     '<a href="https://printmy.blog/user-guide/pro/getting-started/8-generate-a-preview-file/" target="_blank">',
                     '</a>',
                     '<a href="https://printmy.blog/user-guide/pro/getting-started/9-update-the-project/" target="_blank">',
