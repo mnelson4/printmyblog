8057434b95b6a449ca58765bde3dfbb82934a25b
Michael Nelson <thespacecamel@hotmail.com>
2021-05-17 22:28:52 -0700

    maybe fixed footnotes with page references

--- inc/design_functions.php
+++ inc/design_functions.php
@@ -23,6 +23,9 @@ function pmb_design_styles(\PrintMyBlog\orm\entities\Design $design){
 	// page reference CSS
     $css .= '.pmb-posts a.pmb-page-ref[href]::after{
         content: " ' . sprintf($design->getSetting('page_reference_text'),'" target-counter(attr(href), page) "') . '";
+    }
+    .pmb-posts a[href].pmb-page-num::after{
+        content: target-counter(attr(href), page);
     }';
 	// instruct PMB print service to add "powered by" for free users and cheap plans
     if($design->getSetting('powered_by') || ! pmb_fs()->is_plan__premium_only('hobby')){
