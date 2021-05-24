f843753e95d3cbf9cd60c1703fe096cfe6d0641d
Michael Nelson <thespacecamel@hotmail.com>
2021-05-24 12:33:43 -0700

    trying to detect upgrade but struggling

--- src/PrintMyBlog/system/Activation.php
+++ src/PrintMyBlog/system/Activation.php
@@ -8,6 +8,7 @@ use PrintMyBlog\domain\DefaultProjectContents;
 use PrintMyBlog\services\DesignRegistry;
 use Twine\system\RequestType;
 use Twine\system\Activation as BaseActivation;
+use Twine\system\VersionHistory;
 
 /**
  * Class Activation
@@ -45,6 +46,10 @@ class Activation extends BaseActivation
      * @var MigrationManager
      */
     private $migration_manager;
+    /**
+     * @var VersionHistory|null
+     */
+    private $version_history;
 
 
     public function inject(
@@ -53,7 +58,8 @@ class Activation extends BaseActivation
         Capabilities $capabilities = null,
         DesignRegistry $design_registry = null,
         DefaultProjectContents $project_contents = null,
-        MigrationManager $migration_manager = null
+        MigrationManager $migration_manager = null,
+        VersionHistory $version_history = null
     ) {
         parent::inject($request_type);
         $this->table_manager = $table_manager;
@@ -61,6 +67,7 @@ class Activation extends BaseActivation
         $this->design_registry = $design_registry;
         $this->project_contents = $project_contents;
         $this->migration_manager = $migration_manager;
+        $this->version_history = $version_history;
     }
     /**
      * Redirects the user to the blog printing page if the user just activated the plugin and
@@ -69,6 +76,14 @@ class Activation extends BaseActivation
      */
     public function detectActivation()
     {
+        //activation indicator false so it's not newly activated or reactivated
+        // there's no previous version, so it must have been using PMB 2
+        $activation_indicator = get_option('pmb_activation', null);
+        $previous_version = get_option('pmb_previous_version',null);
+
+        //on a brand new install (or deactivate and activate another version), activation indicator will be true
+        //on an upgrade, activation indicator will be false
+        // so if previous version isnt set, and its not an activation it must be an upgrade
         parent::detectActivation();
 //        if ($this->request_type->isBrandNewInstall() && current_user_can(PMB_ADMIN_CAP)) {
 //            // Don't redirect if it's a bulk plugin activation
@@ -78,6 +93,19 @@ class Activation extends BaseActivation
 //            // @todo Do redirection later if we can.
 //            $this->redirectToActivationPage();
 //        }
+        if($activation_indicator === '' && $previous_version === null){
+                wp_redirect(
+                    add_query_arg(
+                        array(
+                            'welcome' => 1,
+                            'upgrade' => 1
+                        ),
+                        admin_url(PMB_ADMIN_PAGE_PATH)
+                    )
+                );
+                exit;
+        }
+
     }
 
 
