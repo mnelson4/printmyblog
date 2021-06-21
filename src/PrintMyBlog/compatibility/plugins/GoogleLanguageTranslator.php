71721e7e285bef8f3d76706ccb860ef5f646ab7a
Michael Nelson <thespacecamel@hotmail.com>
2021-06-21 15:25:17 -0700

    add print styles to avoid unnecessary gogoel trnalsator stuff from appearing in printout

--- src/PrintMyBlog/compatibility/plugins/GoogleLanguageTranslator.php
+++ src/PrintMyBlog/compatibility/plugins/GoogleLanguageTranslator.php
@@ -10,5 +10,21 @@ class GoogleLanguageTranslator extends CompatibilityBase
     {
         global $google_language_translator;
         add_action('wp_enqueue_scripts', array($google_language_translator, 'flags'));
+        add_action('wp_enqueue_scripts', array($this, 'enqueueStyles'));
+    }
+
+    /**
+     * Add a few styles to prevent Google Translator stuff from appearing in the printout
+     */
+    public function enqueueStyles(){
+        wp_add_inline_style(
+            'google-language-translator',
+            '@media print{
+                .goog-te-banner-frame, #glt-translate-trigger{
+                    display:none;
+                }
+            }
+            /* all mike base*/'
+        );
     }
 }
