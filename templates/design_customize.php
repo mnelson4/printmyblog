a5d518b6233fb61655b997635ed0a869c0b08c67
Michael Nelson <thespacecamel@hotmail.com>
2021-07-23 14:00:16 -0700

    call ti save and proceed instead of just save

--- templates/design_customize.php
+++ templates/design_customize.php
@@ -27,7 +27,7 @@ pmb_render_template(
 <div class="pmb-right-help"><span class="dashicons dashicons-admin-site-alt3"></span><a target="_blank" href="<?php echo esc_url($design->getDesignTemplate()->getDocs());?>"><?php printf(esc_html__('Read %s Documentation Online', 'print-my-blog'), $design->getDesignTemplate()->getTitle());?></a></div>
 <form method="POST" action="<?php echo esc_attr($form_url);?>">
     <?php echo $form->getHtmlAndJs();?>
-    <button class="button button-primary pmb-save"><?php esc_html_e('Save', 'print-my-blog');?></button>
+    <button class="button button-primary pmb-save"><?php esc_html_e('Save & Proceed', 'print-my-blog');?></button>
 <!--    <button id="pmb-save-as" class="button button-primary pmb-save-as">--><?php //esc_html_e('Save As...', 'print-my-blog');?><!--</button>-->
 </form>
 <?php pmb_render_template('partials/project_footer.php');
