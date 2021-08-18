aa6155b0e7ef2550d21f5feec4c11ac77f219428
Michael Nelson <thespacecamel@hotmail.com>
2021-08-17 20:43:40 -0700

    aha use length property on jquery selection

--- assets/scripts/print-page-beautifier-functions.js
+++ assets/scripts/print-page-beautifier-functions.js
@@ -202,13 +202,13 @@ function pmb_replace_internal_links_with_page_refs_and_footnotes(external_link_p
         try{
             var matching_elements = jQuery(id_from_href).length;
             if( matching_elements === 0){
-                matching_elements = jQuery(a.attr('href'));
+                matching_elements = jQuery(a.attr('href')).length;
             }
         }catch(exception){
             // somehow the query didn't work. Remove this link then.
             a.contents().unwrap();
         }
-        if( matching_elements.size() > 0){
+        if( matching_elements > 0){
             // internal
             switch(internal_link_policy){
                 case 'parens':
