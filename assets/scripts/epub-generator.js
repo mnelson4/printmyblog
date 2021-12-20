883692223c53e637adfc4be0509ef30e5f979768
Michael Nelson <thespacecamel@hotmail.com>
2021-12-20 15:03:30 -0800

    add TOC to the eright spot (or not at all) and exclude the TOC page from PMB in favour of the one from epub-gen

--- assets/scripts/epub-generator.js
+++ assets/scripts/epub-generator.js
@@ -10,13 +10,33 @@ jQuery(document).ready(function(){
         title: 'test',
         verbose: true,
         ignoreFailedDownloads:true,
+        prependChapterTitles:false,
+        numberChaptersInTOC:false,
     }
     var sections = [];
+    var found_toc = false;
     jQuery('.pmb-section').each(function(index,element){
-        sections.push({
-            content:element.outerHTML
-        });
+        var chapter_data = {content:element.outerHTML, beforeToc: ! found_toc};
+
+        var jqelement = jQuery(element);
+        var section_title = jqelement.find('.pmb-title');
+
+
+        if(section_title.length){
+            chapter_data.title = section_title.text();
+            chapter_data.excludeFromToc = false;
+        } else {
+            chapter_data.excludeFromToc = true;
+        }
+        if(element.id.indexOf('pmb-toc') !== -1){
+            found_toc = true;
+            // don't add the TOC page to the book. epub-gen-memory.js adds it automatically
+            return;
+        }
+        sections.push(chapter_data);
     });
+    epub_options.tocInTOC = found_toc;
+
     const epub = epubGen.default;
     (async () => {
         const content = await epub(epub_options, sections);
