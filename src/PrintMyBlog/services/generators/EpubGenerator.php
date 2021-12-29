18a18640ca5dc081bd7a329050fc21c3f142b798
Michael Nelson <thespacecamel@hotmail.com>
2021-12-29 12:09:00 -0800

    remove svg emojis, and instead just show regular emojis. Kindle doesnt look great with them but it at least works

--- src/PrintMyBlog/services/generators/EpubGenerator.php
+++ src/PrintMyBlog/services/generators/EpubGenerator.php
@@ -4,6 +4,24 @@ namespace PrintMyBlog\services\generators;
 
 class EpubGenerator extends HtmlBaseGenerator
 {
+
+    public function startGenerating(){
+        $this->disableEmojis();
+        parent::startGenerating();
+    }
+
+    /**
+     * Emojis break Amazon Kindle Previewer
+     */
+    protected function disableEmojis(){
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+        remove_action( 'admin_print_styles', 'print_emoji_styles' );
+        remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+        remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    }
     /**
      * Writes out the PMB Pro print "window" which appears at the top of pro print pages.
      * Echoes, instead of using `$this->file_writer`, because this is a callback on an action called inside the template HTML.
