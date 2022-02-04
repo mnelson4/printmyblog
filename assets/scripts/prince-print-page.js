32fe3cd180a95dbefa408a4ec3ae7172ed6556b4
Michael Nelson <thespacecamel@hotmail.com>
2022-02-04 11:45:26 -0800

    try again to resize width

--- assets/scripts/prince-print-page.js
+++ assets/scripts/prince-print-page.js
@@ -7,11 +7,10 @@ Prince.registerPostLayoutFunc(function() {
         var box = x.getPrinceBoxes()[0];
         var p = PDF.pages[box.pageNum-1];
 
-        var old_height = p.h;
+        var old_height = box.h;
         var new_height = box.y - (p.y - p.h) - 10;
         var ratio = old_height / new_height;
 
-        var old_width = p.w;
         var new_width = p.w * ratio;
 
         x.style.height = new_height + "pt";
