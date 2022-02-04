48d92208e34f480a3411c76422914fa92d11a5a0
Michael Nelson <thespacecamel@hotmail.com>
2022-02-04 11:25:08 -0800

    experiment to debug3

--- assets/scripts/prince-print-page.js
+++ assets/scripts/prince-print-page.js
@@ -1,12 +1,12 @@
 Prince.trackBoxes = true;
 
 Prince.registerPostLayoutFunc(function() {
-    // var xs = document.getElementsByClassName("pmb-dynamic-resize");
+    var xs = document.getElementsByClassName("pmb-dynamic-resize");
     for (var i = 0; i < xs.length; ++i) {
-        var x = xs[i].getElementsByTagName("img")[0];
-        var box = x.getPrinceBoxes()[0];
-        var p = PDF.pages[box.pageNum-1];
-        var h = box.y - (p.y - p.h) - 10;
-        x.style.height = h + "pt";
+        // var x = xs[i].getElementsByTagName("img")[0];
+        // var box = x.getPrinceBoxes()[0];
+        // var p = PDF.pages[box.pageNum-1];
+        // var h = box.y - (p.y - p.h) - 10;
+        // x.style.height = h + "pt";
     }
 });
