270156e26bb71fce0f619d7e98f910a7664b595a
Michael Nelson <thespacecamel@hotmail.com>
2022-02-14 15:24:19 -0800

    try rendering between each resize

--- assets/scripts/prince-print-page.js
+++ assets/scripts/prince-print-page.js
@@ -6,33 +6,35 @@ Prince.trackBoxes = true;
 // once the first pass of rendering is finished, let's make the "pmb-dynamic-resize" images take up the rest of the
 // page they're on. Prince will then need to re-render.
 Prince.registerPostLayoutFunc(function() {
-
     var elements_to_resize = document.getElementsByClassName("pmb-dynamic-resize");
-    for (var i = 0; i < elements_to_resize.length; ++i) {
-
-        // get the image's size, and how far it is from the bottom of the page
-        // then stretch it down to there.
-        var element_to_resize = elements_to_resize[i];
-        element_to_resize.className += ' pmb-dynamic-resize-done';
-        var element_box = element_to_resize.getPrinceBoxes()[0];
-        var page_box = PDF.pages[element_box.pageNum-1];
-
-        // don't forget to take the footnote height into account
-        var footnotes_height = 0;
-        for (var index in page_box['children']){
-            var box_on_page = page_box['children'][index];
-            if(box_on_page['type'] === 'FOOTNOTES'){
-                footnotes_height = box_on_page['h'];
-            }
-        }
-        Log.info('=========' + i + '=================');
-        pmb_print_props(element_box, '>>> element ' + i + ' box');
-        pmb_print_props(page_box, '>>> page ' + (element_box.pageNum-1) + ' box');
-        var new_element_height = element_box.y - (page_box.y - page_box.h) - 10 - footnotes_height;
-        element_to_resize.style.height = new_element_height + "pt";
-        Log.info('new element height:' + new_element_height);
-    }
+    pmb_continue_image_resizing();
 });
+
+function pmb_continue_image_resizing(){
+    var element_to_resize = elements_to_resize.shift();
+    if(typeof element_to_resize === 'undefined'){
+        return;
+    }
+    element_to_resize.className += ' pmb-dynamic-resize-done';
+    var element_box = element_to_resize.getPrinceBoxes()[0];
+    var page_box = PDF.pages[element_box.pageNum-1];
+
+    // don't forget to take the footnote height into account
+    var footnotes_height = 0;
+    for (var index in page_box['children']){
+        var box_on_page = page_box['children'][index];
+        if(box_on_page['type'] === 'FOOTNOTES'){
+            footnotes_height = box_on_page['h'];
+        }
+    }
+    Log.info('=========' + i + '=================');
+    pmb_print_props(element_box, '>>> element ' + i + ' box');
+    pmb_print_props(page_box, '>>> page ' + (element_box.pageNum) + ' box');
+    var new_element_height = element_box.y - (page_box.y - page_box.h) - 10 - footnotes_height;
+    element_to_resize.style.height = new_element_height + "pt";
+    Log.info('new element height:' + new_element_height);
+    Prince.registerPostLayoutFunc(pmb_continue_image_resizing);
+}
 
 /**
  * A debugging function, especially useful for figuring out what's on these "box" objects
