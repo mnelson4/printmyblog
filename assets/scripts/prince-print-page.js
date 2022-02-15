56596ce9690f165bdfbc0c201dfbba312696e3cf
Michael Nelson <thespacecamel@hotmail.com>
2022-02-15 08:28:43 -0800

    make selectors a little less aggressive in case designs want to override them

--- assets/scripts/prince-print-page.js
+++ assets/scripts/prince-print-page.js
@@ -19,7 +19,6 @@ function pmb_continue_image_resizing(){
     element_to_resize.className = element_to_resize.className.replace('pmb-dynamic-resize', '') + ' pmb-dynamic-resize-done';
     var element_box = element_to_resize.getPrinceBoxes()[0];
     var page_box = PDF.pages[element_box.pageNum-1];
-    var sub_image = element_to_resize.getElementsByTagName("img")[0];
 
     // don't forget to take the footnote height into account
     var footnotes_height = 0;
