9467889f40761cfeda0b0bbeba6329255cd9e16c
Michael Nelson <thespacecamel@hotmail.com>
2022-03-10 13:00:52 -0800

    ok going back in release versions confues wordpress (and everyone). Just name them normally

--- printmyblog.php
+++ printmyblog.php
@@ -9,7 +9,7 @@
  * Description: Make printing your blog easy and impressive. For you & your visitors. One post or thousands.
  * Author: Michael Nelson
  * Author URI: https://printmy.blog
- * Version: 3.12.3
+ * Version: 3.12.5
  * Requires at least: 4.7
  * Requires PHP: 5.4
  * Text Domain: print-my-blog
@@ -119,7 +119,7 @@ if (defined('PMB_VERSION')) {
 
 } else {
     // it's all good! go for it!
-    define('PMB_VERSION', '3.12.3');
+    define('PMB_VERSION', '3.12.5');
     define('PMB_DIR', wp_normalize_path(__DIR__) . '/');
     define('PMB_MAIN_FILE', __FILE__);
     define('PMB_TEMPLATES_DIR', PMB_DIR . 'templates/');
