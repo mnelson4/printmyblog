a8f607c3bde87d272d9e4fdacbd2a1d13219d632
Michael Nelson <thespacecamel@hotmail.com>
2022-04-08 11:53:37 -0700

    image resizing with captions finally looks good. remove etra debugging ingo

--- assets/scripts/prince-print-page.js
+++ assets/scripts/prince-print-page.js
@@ -74,7 +74,6 @@ function pmb_resize_an_image_inside(element){
     var caption_height = 0;
     if(typeof(figure_caption) !== 'undefined'){
         var caption_box = figure_caption.getPrinceBoxes()[0];
-        pmb_print_props(caption_box, 'caption box');
         caption_height = caption_box.marginTop + caption_box.h + caption_box.marginBottom;
     }
     // If we can't find an image to resize, there's nothing to resize (which is weird but somehow happens?)
