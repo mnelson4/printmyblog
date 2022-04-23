ed0975adecd0e0784c18f9f1b145f085fc447c4b
Michael Nelson <thespacecamel@hotmail.com>
2022-04-23 13:44:35 -0700

    use actual epub filename 2

--- assets/scripts/epub-generator.js
+++ assets/scripts/epub-generator.js
@@ -77,7 +77,7 @@ jQuery(document).on('pmb_wrap_up', function(){
                 .then(() => console.log('done writing'))
         }
 
-        const fileStream = streamSaver.createWriteStream(jQuery('#download_link').attr('download').val(), {
+        const fileStream = streamSaver.createWriteStream(jQuery('#download_link').attr('download').valueOf(), {
             size: blob.size // Makes the procentage visiable in the download
         })
 
