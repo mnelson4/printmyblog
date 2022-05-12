cb7661f024847ca0992734654a5096561348ff50
Michael Nelson <thespacecamel@hotmail.com>
2022-05-12 09:27:30 -0700

    page break except in front of first item

--- designs/word/classic/templates/part.php
+++ designs/word/classic/templates/part.php
@@ -1,5 +1,12 @@
 <div <?php pmb_section_wrapper_class();?> <?php pmb_section_wrapper_id();?>>
-
+    <?php
+    // page break unless this is the first section
+    if ($post->pmb_section->getSectionOrder() != 1){
+    ?>
+    <br clear=all style='mso-special-character:line-break;page-break-before:always'>
+    <?php
+    }
+    ?>
     <article <?php pmb_section_class(); ?> <?php pmb_section_id(); ?>>
 
         <header class="entry-header has-text-align-center">
