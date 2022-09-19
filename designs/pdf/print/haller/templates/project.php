9fd7c0933c766373ce279aa89e5983fa9262e5bb
Michael Nelson <thespacecamel@hotmail.com>
2022-09-19 10:44:06 -0700

    tabloid work2

--- designs/pdf/print/haller/templates/project.php
+++ designs/pdf/print/haller/templates/project.php
@@ -24,8 +24,9 @@ echo str_replace('has-sidebar', '', implode(' ', get_body_class('pmb-print-page 
     <div id="haller-repeat-header-title">
         <?php echo $pmb_design->getSetting('publication_title'); ?>
     </div>
-    <div>
-        <div class="haller-centered-inner">Page <span class="pmb-page-number"></span>
+    <div class=""haller-centered">
+        <div class="haller-centered-inner">
+            Page <span class="pmb-page-number"></span>
         </div>
     </div>
 </div>
