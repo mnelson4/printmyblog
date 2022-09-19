fc973e4d15fbf9b526e8d6dd8b7076732221adb5
Michael Nelson <thespacecamel@hotmail.com>
2022-09-19 10:47:51 -0700

    tabloid work4

--- designs/pdf/print/haller/templates/project.php
+++ designs/pdf/print/haller/templates/project.php
@@ -15,19 +15,13 @@ echo str_replace('has-sidebar', '', implode(' ', get_body_class('pmb-print-page 
 <?php do_action('pmb_pro_print_window'); ?>
 <div class="pmb-posts pmb-project-content site">
 <div id="haller-repeat-header">
-    <div class="haller-centered">
-        <div class="haller-centered-inner">
-            <?php $pmb_project->echoSetting('date');?>
-        </div>
+    <div>
+        <?php $pmb_project->echoSetting('date');?>
     </div>
-    <div id="haller-repeat-header-title" class="haller-centered">
-        <div class="haller-centered-inner">
-            <?php echo $pmb_design->getSetting('publication_title'); ?>
-        </div>
+    <div id="haller-repeat-header-title" >
+        <?php echo $pmb_design->getSetting('publication_title'); ?>
     </div>
-    <div class="haller-centered">
-        <div class="haller-centered-inner">
-            <?php $pmb_project->echoSetting('issue');?>, Page <span class="pmb-page-number"></span>
-        </div>
+    <div>
+        <?php $pmb_project->echoSetting('issue');?>, Page <span class="pmb-page-number"></span>
     </div>
 </div>
