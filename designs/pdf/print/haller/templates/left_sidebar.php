ee2b953d65e8cd0c11f7c116f86db1570479bf38
Michael Nelson <thespacecamel@hotmail.com>
2022-09-22 10:59:31 -0700

    Add title to sidebars

--- designs/pdf/print/haller/templates/left_sidebar.php
+++ designs/pdf/print/haller/templates/left_sidebar.php
@@ -1,4 +1,9 @@
 <div <?php pmb_section_wrapper_class('haller-sidebar left pmb-fill-remaining-height'); ?> <?php pmb_section_wrapper_id(); ?>>
+    <header class="entry-header has-text-align-center">
+        <div class="entry-header-inner section-inner medium">
+            <?php pmb_the_title();?>
+        </div>
+    </header>
     <article <?php pmb_section_class(); ?> <?php pmb_section_id(); ?>>
         <?php pmb_include_design_template('partials/content'); ?>
     </article>
