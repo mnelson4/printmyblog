71a0ee4d517a08db760bbdfe4bd28b2f14d28729
Michael Nelson <thespacecamel@hotmail.com>
2022-12-21 00:46:36 -0800

    3.20.3 fixed plugin icon missing on updates page and epub load order issue

--- designs/epub/classic/assets/script.js
+++ designs/epub/classic/assets/script.js
@@ -1,3 +1,6 @@
+var pmb_window_load_fired = false;
+var pmb_done_document_ready_tasks = false;
+
 jQuery(document).ready(function(){
     console.log('PMB says document ready ');
     // Pretty up the page
@@ -5,11 +8,29 @@ jQuery(document).ready(function(){
     pmb_default_align_center();
     pmb_replace_internal_links_with_epub_file_links();
     pmb_add_alt_tags();
+    pmb_done_document_ready_tasks = true;
+    pmb_epub_maybe_process_after_images_ready();
 });
 
 // wait until the images are loaded to try to resize them.
 jQuery(window).on("load", function() {
     console.log('PMB says window loaded ');
+    pmb_window_load_fired = true;
+    pmb_epub_maybe_process_after_images_ready();
+});
+
+/**
+ * We can't be certain is document ready or window load will be called first (see https://stackoverflow.com/questions/47405458/which-come-first-document-load-or-window-load)
+ * so we need to make sure this is called after they've both ran (because on document ready we disable lazy-loading,
+ * and on window load we know non-lazy-loaded images are all available, so we need to wait for both before processing
+ * those images.)
+ */
+function pmb_epub_maybe_process_after_images_ready(){
+    // only execute once window loaded (images loaded) and PMB's done all the ready tasks.
+    // We can't be certain on what order those will be executed so this gets called twice.
+    if(! pmb_window_load_fired || !pmb_done_document_ready_tasks){
+        return;
+    }
     // pmb_resize_images(parseInt(pmb_design_options['image_size'],10));
     pmb_change_image_quality(pmb_design_options.image_quality, pmb_design_options.domain);
 
@@ -17,15 +38,21 @@ jQuery(window).on("load", function() {
         jQuery(document).trigger('pmb_wrap_up');
     });
     if(pmb_design_options.convert_videos === '1'){
-        // if processing videos, wait until that's done to replace images.
-        jQuery(document).on('pmb_done_processing_videos', function(){
-            var erc = new PmbExternalResourceCacher();
-            erc.replaceExternalImages();
-        });
-        pmb_convert_youtube_videos_to_images('simple');
+        // wait a couple seconds for Elementor videos to convert from DIVs to iframes
+        setTimeout(
+            function(){
+                // if processing videos, wait until that's done to replace images.
+                jQuery(document).on('pmb_done_processing_videos', function(){
+                    var erc = new PmbExternalResourceCacher();
+                    erc.replaceExternalImages();
+                });
+                pmb_convert_youtube_videos_to_images('simple');
+            },
+            2000
+        );
+
     } else {
         var erc = new PmbExternalResourceCacher();
         erc.replaceExternalImages();
     }
-
-});
+}
--- readme.txt
+++ readme.txt
@@ -7,7 +7,7 @@ Requires PHP: 5.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Donate link: https://printmy.blog/pricing/
-Stable tag: 3.20.2
+Stable tag: 3.20.3
 
 Make printing your blog easy and impressive. For you & your visitors. One post or thousands.
 
@@ -438,6 +438,10 @@ https://youtu.be/uPQ3vqm7A2g
 8. Pro Print digital PDF using Buurma Whitepaper design. The design adds a background color with a gradient, and a custom logo behind the page number. Designers can use the full power of HTML and CSS in their PDF designs!
 
 == Changelog ==
+
+= 3.20.3 December 21, 2022 =
+* Bugfix: in Pro Print, fix error creating ePub when page loaded in unexpected order
+* Bugfix: fix PMB's icon appearing in the updates page
 
 = 3.20.2 December 20, 2022 =
 * Bugfix: in Pro Print, fix protocols of URLs in inline styles (e.g. as with Advanced Ads plugin)
