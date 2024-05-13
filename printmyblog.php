489b612fed0bb1c31f3e9e2ba00cbbc30931f8db
mnelson4 <thespacecamel@hotmail.com>
2024-05-13 10:55:34 -0700

    actually call it beta in case I change it before an actual releas

--- printmyblog.php
+++ printmyblog.php
@@ -9,7 +9,7 @@
  * Description: Make printing your blog easy and impressive. For you & your visitors. One post or thousands.
  * Author: Michael Nelson
  * Author URI: https://printmy.blog
- * Version: 3.27.0
+ * Version: 3.27.0.beta.001
  * Requires at least: 4.7
  * Requires PHP: 5.4
  * Text Domain: print-my-blog
